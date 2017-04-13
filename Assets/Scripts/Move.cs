9984af042fc9f410f3c6939171a907ab5b3f109f
Will McMain <willmcmain@gmail.com>
2017-04-12 22:17:02 -0400

    Getting started
    Go little square!

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+	public float speed = 0.1f;
+
+	private IEnumerator moveRoutine;
+
+	private IEnumerator MoveRoutine(Vector2 dest) {
+		while (Vector2.Distance (transform.position, dest) > float.Epsilon) {
+			Vector2 path = (dest - (Vector2)transform.position);
+			Vector2 movement = path.normalized * speed;
+			if (movement.magnitude > path.magnitude) {
+				transform.position = dest;
+			} else {
+				transform.position += (Vector3)(path.normalized * speed);
+			}
+			yield return null;
+		}
+	}
+
+	public void MoveTo(Vector2 dest) {
+		if (moveRoutine != null) {
+			StopCoroutine (moveRoutine);
+		}
+		moveRoutine = MoveRoutine (dest);
+		StartCoroutine(moveRoutine);
+	}
+}
---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour {
+	public GameObject selected;
+	[SerializeField] private Vector2 destination;
+
+	void Update() {
+		if (Input.GetButtonDown ("Order")) {
+			destination = Camera.main.ViewportToWorldPoint (
+				Camera.main.ScreenToViewportPoint (Input.mousePosition));
+
+			//selected.transform.position = destination;
+
+			Move move = selected.GetComponent<Move> ();
+			move.MoveTo (destination);
+		}
+	}
+}
